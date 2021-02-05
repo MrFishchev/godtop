875a7fbf1be951ce4851fa5286801c4df9f79c8c
mrfishchev <dopingoo@gmail.com>
2021-02-06 01:04:17 +0300

    Migrate to gin server

--- go.mod
+++ go.mod
@@ -9,14 +9,33 @@ require (
 	github.com/docker/docker v20.10.2+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/julienschmidt/httprouter v1.3.0
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/moby/term v0.0.0-20201216013528-df9cb8a40635 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/shirou/gopsutil/v3 v3.21.1
 	github.com/sirupsen/logrus v1.7.0 // indirect
-	github.com/takashabe/go-router v0.0.0-20180130155705-1eefa4c80f55
 	github.com/tidwall/gjson v1.6.8
+	github.com/ugorji/go v1.2.4 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	google.golang.org/grpc v1.35.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+	gotest.tools/v3 v3.0.3 // indirect
 )
--- interfaces/handler.go
+++ interfaces/handler.go
@@ -1,7 +1,6 @@
 package interfaces
 
 import (
-	"encoding/json"
 	"fmt"
 	"godtop/application"
 	"godtop/domain"
@@ -9,7 +8,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gin-gonic/gin"
 )
 
 func logDebug(format string, args ...interface{}) {
@@ -29,53 +28,18 @@ func (e *ErrorResponse) ToString() string {
 }
 
 //Error is wrapped Respond when error
-func Error(w http.ResponseWriter, code int, err error, msg string) {
+func Error(ctx *gin.Context, code int, err error, msg string) {
 	e := &ErrorResponse{
 		Message: msg,
 		Error:   err,
 	}
 
 	logDebug("%s", e.ToString())
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	Respond(w, code, e)
+	ctx.JSON(code, e)
 }
 
-func Ok(w http.ResponseWriter, code int, src ...interface{}) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	Respond(w, code, src)
-}
-
-//Respond writes response to ResponseWriter
-func Respond(w http.ResponseWriter, code int, src interface{}) {
-	var body []byte
-	var err error
-
-	switch s := src.(type) {
-	case []byte:
-		if !json.Valid(s) {
-			Error(w, http.StatusInternalServerError, err, "Invalid json")
-			return
-		}
-		body = s
-	case string:
-		body = []byte(s)
-	case *ErrorResponse, ErrorResponse:
-		//if cannot parse json of ErrorResponse, avoid inifinite loop
-		if body, err = json.Marshal(src); err != nil {
-			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("{\"reason\":\"Failed to parse json\"}"))
-			return
-		}
-	default:
-		if body, err = json.Marshal(src); err != nil {
-			Error(w, http.StatusInternalServerError, err, "Failed to parse json")
-			return
-		}
-	}
-
-	w.WriteHeader(code)
-	w.Write(body)
+func Ok(ctx *gin.Context, src ...interface{}) {
+	ctx.JSON(http.StatusOK, src)
 }
 
 //Handler docker service
@@ -85,8 +49,8 @@ type Handler struct {
 }
 
 //Routes returns the initialized router
-func (h Handler) Routes() *httprouter.Router {
-	r := httprouter.New()
+func (h Handler) Routes() *gin.Engine {
+	r := gin.Default()
 	r.GET("/containers", h.getRunningContainers)
 	r.GET("/containers/all", h.getAllContainers)
 	r.GET("/container/:nameOrId", h.getContainer)
@@ -99,24 +63,21 @@ func (h Handler) Routes() *httprouter.Router {
 //RunServer starts server on a specific port
 func (h Handler) RunServer(port int) error {
 	log.Printf("Server running at http://localhost:%d/", port)
-	return http.ListenAndServe(fmt.Sprintf(":%d", port), h.Routes())
+	return h.Routes().Run(fmt.Sprintf(":%d", port))
 }
 
-func (h Handler) getHostInfo(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	ctx := r.Context()
-
+func (h Handler) getHostInfo(ctx *gin.Context) {
 	interactor := application.HostInteractor{
 		Service: h.HostService,
 	}
 
 	info := interactor.GetInfo(ctx)
 
-	Ok(w, http.StatusOK, info)
+	Ok(ctx, info)
 }
 
-func (h Handler) getContainer(w http.ResponseWriter, r *http.Request, args httprouter.Params) {
-	nameOrId := args.ByName("nameOrId")
-	ctx := r.Context()
+func (h Handler) getContainer(ctx *gin.Context) {
+	nameOrId := ctx.Param("nameOrId")
 
 	interactor := application.ContainerInteractor{
 		Service: h.DockerService,
@@ -124,25 +85,22 @@ func (h Handler) getContainer(w http.ResponseWriter, r *http.Request, args httpr
 
 	container, err := interactor.Get(ctx, nameOrId)
 	if err != nil {
-		Error(w, http.StatusNotFound, err, err.Error())
+		Error(ctx, http.StatusNotFound, err, err.Error())
 		return
 	}
 
-	Ok(w, http.StatusOK, container)
+	Ok(ctx, container)
 }
 
-func (h Handler) getRunningContainers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	h.getContainers(&w, r, false)
+func (h Handler) getRunningContainers(ctx *gin.Context) {
+	h.getContainers(ctx, false)
 }
 
-func (h Handler) getAllContainers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	h.getContainers(&w, r, true)
+func (h Handler) getAllContainers(ctx *gin.Context) {
+	h.getContainers(ctx, true)
 }
 
-func (h Handler) getContainers(w *http.ResponseWriter, r *http.Request, all bool) {
-
-	ctx := r.Context()
-
+func (h Handler) getContainers(ctx *gin.Context, all bool) {
 	interactor := application.ContainerInteractor{
 		Service: h.DockerService,
 	}
@@ -156,18 +114,17 @@ func (h Handler) getContainers(w *http.ResponseWriter, r *http.Request, all bool
 	}
 
 	if err != nil {
-		Error(*w, http.StatusNotFound, err, err.Error())
+		Error(ctx, http.StatusNotFound, err, err.Error())
 		return
 	}
 	type payload struct {
 		Containers *[]domain.Container `json:"containers"`
 	}
-	Ok(*w, http.StatusOK, payload{Containers: containers})
+	Ok(ctx, payload{Containers: containers})
 }
 
-func (h Handler) getContainerStats(w http.ResponseWriter, r *http.Request, args httprouter.Params) {
-	nameOrId := args.ByName("nameOrId")
-	ctx := r.Context()
+func (h Handler) getContainerStats(ctx *gin.Context) {
+	nameOrId := ctx.Param("nameOrId")
 
 	interactor := application.ContainerInteractor{
 		Service: h.DockerService,
@@ -175,7 +132,7 @@ func (h Handler) getContainerStats(w http.ResponseWriter, r *http.Request, args 
 
 	stats, err := interactor.GetStats(ctx, nameOrId, false)
 	if err != nil {
-		Error(w, http.StatusNotFound, err, err.Error())
+		Error(ctx, http.StatusNotFound, err, err.Error())
 		return
 	}
 
@@ -183,19 +140,17 @@ func (h Handler) getContainerStats(w http.ResponseWriter, r *http.Request, args 
 		Stats *domain.ContainerStats `json:"stats"`
 	}
 
-	Ok(w, http.StatusOK, payload{Stats: stats})
+	Ok(ctx, payload{Stats: stats})
 }
 
-func (h Handler) getVolumes(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	ctx := r.Context()
-
+func (h Handler) getVolumes(ctx *gin.Context) {
 	interactor := application.VolumeInteractor{
 		Service: h.DockerService,
 	}
 
 	volumes, err := interactor.GetAll(ctx)
 	if err != nil {
-		Error(w, http.StatusNotFound, err, err.Error())
+		Error(ctx, http.StatusNotFound, err, err.Error())
 		return
 	}
 
@@ -203,5 +158,5 @@ func (h Handler) getVolumes(w http.ResponseWriter, r *http.Request, _ httprouter
 		Volumes *[]domain.Volume `json:"volumes"`
 	}
 
-	Ok(w, http.StatusOK, payload{Volumes: volumes})
+	Ok(ctx, payload{Volumes: volumes})
 }
